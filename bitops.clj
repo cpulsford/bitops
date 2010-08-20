74a5714b62a9b94562a867cadfa7be27163773a2
Cameron Pulsford <cpulsfor@gmail.com>
2010-08-20 00:17:48 -0400

    Reworked some functions around the assumption of >>>

--- bitops.clj
+++ bitops.clj
@@ -5,8 +5,23 @@
 
 (ns binary)
 
+;; For consistency with new >>> operator
+(defn <<
+  "Arithmetic bitwise shift left"
+  {:inline (fn [x n] `(. clojure.lang.Numbers (shiftLeft ~x ~n)))}
+  [x n]
+  (. clojure.lang.Numbers shiftLeft x n))
+
+(defn >>
+  "Arithmetic bitwise shift right"
+  {:inline (fn [x n] `(. clojure.lang.Numbers (shiftRight ~x ~n)))}
+  [x n]
+  (. clojure.lang.Numbers shiftRight x n))
+
+
+
 ;;
-;; Integer (whole number) specific bit operations.
+;; Integer (whole number) specific bit operations
 ;;
 
 (defprotocol IntegerBitOps
@@ -22,8 +37,10 @@
   (bit-rotate-left [x n] (Long/rotateLeft x n))
   (bit-rotate-right [x n] (Long/rotateRight x n)))
 
+
+
 ;;
-;; General bit operations.
+;; General bit operations
 ;;
 
 (defprotocol GeneralBitOps
@@ -38,7 +55,7 @@
    :bit-count (fn ([x] (- 32 (Integer/numberOfLeadingZeros x)))
                   ([x radix] (-> (Math/abs x) (to-base radix) count)))})
 
-;; Returns a common implementation of bit-count.
+;; Returns a common implementation of bit-count
 (defn- b-count [] {:bit-count (fn ([x] (-> x to-binary count))
                                   ([x radix] (-> (Math/abs x) (to-base radix) count)))})
 
@@ -66,26 +83,26 @@
          {:to-binary (fn [x] (-> x Double/doubleToLongBits Long/toBinaryString))
           :to-base (fn [x radix] (-> x Double/doubleToLongBits (Long/toString radix)))}))
 
+
+
 ;;
-;; Type agnostic bit functions.
+;; Non-type specific bit functions
 ;;
 
 (defn mask
   "Returns a number that represents a mask of the given length.
    Optionally, a (left) shift amount may be supplied."
   ([n]
-   (-> (bit-shift-left 1 n) dec))
+   (-> (<< 1 n) dec))
   ([n shift]
-   (-> (bit-shift-left 1 n) dec (bit-shift-left shift))))
+   (-> (<< 1 n) dec (<< shift))))
 
 (defn isolate-range
   "Returns the number represented by isolating arg at the given bit
    for the given length. If a length to isolate by is not given, the
    rest of the bit string will be used."
-  ([x n]
-   (-> (bit-count x) (- n) inc (mask n) (bit-and x) (bit-shift-right n)))
-  ([x n len]
-   (-> (mask len n) (bit-and x) (bit-shift-right n))))
+  ([x n] (-> (bit-count x) (- n) mask (bit-and (>>> x n))))
+  ([x n len] (-> x (>>> n) (bit-and (mask len)))))
 
 (defn compose
   "Compose x into the base number at the given bit. If a length is
@@ -93,30 +110,7 @@
    greater than length. The base number is positioned first in the arg
    list to allow for composition chains."
   ([base x n]
-   (-> (bit-count x) (mask n) bit-not (bit-and base) (bit-or (bit-shift-left x n))))
+   (-> (bit-count x) (mask n) bit-not (bit-and base) (bit-or (<< x n))))
   ([base x n len]
    {:pre [(<= (bit-count x) len)]}
    (compose base x n)))
-
-;;
-;; The following functions have the same semantics as their Java counterparts.
-;; 
-;; The defintions of << and >> have been ripped from core for speed.
-;;
-
-(defn <<
-  "Arithmetic bitwise shift left"
-  {:inline (fn [x n] `(. clojure.lang.Numbers (shiftLeft ~x ~n)))}
-  [x n]
-  (. clojure.lang.Numbers shiftLeft x n))
-
-(defn >>
-  "Arithmetic bitwise shift right"
-  {:inline (fn [x n] `(. clojure.lang.Numbers (shiftRight ~x ~n)))}
-  [x n]
-  (. clojure.lang.Numbers shiftRight x n))
-
-(defn >>>
-  "Bitwise logical right shift."
-  [x n]
-  (isolate-range x n (- (bit-count x) n)))
